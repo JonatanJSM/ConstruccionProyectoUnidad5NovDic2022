aad81b83bc7b462605444329d561b6bc8a27f1b6
Jonatan <jsantanamay@gmail.com>
2022-11-11 23:23:37 -0600

    Agregando @Before en test y cambiando path

--- test/ValidarLecturaTest.java
+++ test/ValidarLecturaTest.java
@@ -1,27 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
- */
-
 import Utils.ValidarLectura;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
  * @author fer98
  */
 public class ValidarLecturaTest {
-    private String path = "C:\\Users\\fer98\\Documents\\NetBeansProjects\\validacionJSON\\src\\validacionjson\\json2.txt";
+    private String path = "json2.txt";
+    private static ValidarLectura validacionJSON;
     
     public ValidarLecturaTest() {
+    }
+    
+    @Before
+    public void setUp(){
+        validacionJSON = new ValidarLectura();
     }
 
     @Test
     public void testReadJSON() {
-        ValidarLectura validacionJSON = new ValidarLectura();
         Object obj = validacionJSON.readJSON(path);
         System.out.println(obj);
         assertFalse(obj == null);
@@ -29,7 +30,6 @@ public class ValidarLecturaTest {
     
     @Test
     public void testExtractJSONObject() {
-        ValidarLectura validacionJSON = new ValidarLectura();
         JSONObject jsonObject = validacionJSON.extractJSONObject(validacionJSON.readJSON(path));
         System.out.println(jsonObject);
         assertFalse(jsonObject == null);
@@ -37,7 +37,6 @@ public class ValidarLecturaTest {
     
     @Test
     public void testExtractJSONObjectbyKey() {
-        ValidarLectura validacionJSON = new ValidarLectura();
         JSONObject employeeObject = new JSONObject();
         employeeObject = validacionJSON.extractJSONObjectbyKey(validacionJSON.extractJSONObject(validacionJSON.readJSON(path)), "employees");
         System.out.println(employeeObject);
@@ -46,7 +45,6 @@ public class ValidarLecturaTest {
     
     @Test
     public void testExtractEmployeeJSONArray() {
-        ValidarLectura validacionJSON = new ValidarLectura();
         JSONObject employeeObject = validacionJSON.extractJSONObjectbyKey(validacionJSON.extractJSONObject(validacionJSON.readJSON(path)), "employees");
         JSONArray employeeArray = validacionJSON.extractEmployeeJSONArray(employeeObject, "employee");
         System.out.println(employeeArray); 
